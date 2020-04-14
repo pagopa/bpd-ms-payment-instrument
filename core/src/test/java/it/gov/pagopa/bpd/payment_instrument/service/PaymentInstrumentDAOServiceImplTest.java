7650ffc605ff7b3549a830c01d4cc05ceef31aa3
mdesogus <michael.desogus@everis.com>
2020-04-14 15:34:55 +0200

    test control if paymentInstrument exist before perform delete method

--- core/src/test/java/it/gov/pagopa/bpd/payment_instrument/service/PaymentInstrumentDAOServiceImplTest.java
+++ core/src/test/java/it/gov/pagopa/bpd/payment_instrument/service/PaymentInstrumentDAOServiceImplTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.mockito.exceptions.verification.NeverWantedButInvoked;
 import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -27,6 +28,8 @@ import java.util.Optional;
 @TestPropertySource(properties = "numMaxPaymentInstr=5")
 public class PaymentInstrumentDAOServiceImplTest {
 
+    private static final String HASH_PAN = "hashPan";
+
     @MockBean
     private PaymentInstrumentDAO paymentInstrumentDAOMock;
     @MockBean
@@ -39,7 +42,7 @@ public class PaymentInstrumentDAOServiceImplTest {
     public void initTest() {
         Mockito.reset(paymentInstrumentDAOMock, paymentInstrumentHistoryDAOMock);
 
-        BDDMockito.when(paymentInstrumentDAOMock.findById(Mockito.eq("prova"))).thenAnswer((Answer<PaymentInstrument>)
+        BDDMockito.when(paymentInstrumentDAOMock.findById(Mockito.eq(HASH_PAN))).thenAnswer((Answer<PaymentInstrument>)
                 invocation -> {
                     PaymentInstrument pi = new PaymentInstrument();
                     return pi;
@@ -58,38 +61,50 @@ public class PaymentInstrumentDAOServiceImplTest {
         BDDMockito.when(paymentInstrumentDAOMock.save(Mockito.eq(paymentInstrument))).thenAnswer((Answer<PaymentInstrument>)
                 invocation -> paymentInstrument);
 
-        BDDMockito.when(paymentInstrumentHistoryDAOMock.checkActive(Mockito.eq("test"), Mockito.any()))
+        BDDMockito.when(paymentInstrumentHistoryDAOMock.checkActive(Mockito.eq(HASH_PAN), Mockito.any()))
                 .thenAnswer((Answer<List<PaymentInstrument>>) invocation -> new ArrayList<>());
     }
 
 
     @Test
     public void find() {
-        Optional<PaymentInstrument> paymentInstrument = paymentInstrumentDAOService.find("test");
+        Optional<PaymentInstrument> paymentInstrument = paymentInstrumentDAOService.find(HASH_PAN);
+
         Assert.assertNotNull(paymentInstrument.orElse(null));
-        BDDMockito.verify(paymentInstrumentDAOMock).findById(Mockito.eq("test"));
+        BDDMockito.verify(paymentInstrumentDAOMock).findById(Mockito.eq(HASH_PAN));
     }
 
     @Test
     public void update() {
         PaymentInstrument paymentInstrument = new PaymentInstrument();
-        paymentInstrumentDAOService.update("test", paymentInstrument);
+        paymentInstrumentDAOService.update(HASH_PAN, paymentInstrument);
+
         Assert.assertNotNull(paymentInstrument);
         BDDMockito.verify(paymentInstrumentDAOMock).count(Mockito.eq(Example.of(paymentInstrument)));
-        paymentInstrument.setHpan("test");
+        paymentInstrument.setHpan(HASH_PAN);
         BDDMockito.verify(paymentInstrumentDAOMock).save(Mockito.eq(paymentInstrument));
     }
 
     @Test
-    public void delete() {
-        paymentInstrumentDAOService.delete("prova");
-        BDDMockito.verify(paymentInstrumentDAOMock).findById(Mockito.eq("prova"));
+    public void deleteOK() {
+        paymentInstrumentDAOService.delete(HASH_PAN);
+
+        BDDMockito.verify(paymentInstrumentDAOMock).findById(Mockito.eq(HASH_PAN));
         BDDMockito.verify(paymentInstrumentDAOMock).count(Mockito.any(Example.class));
         BDDMockito.verify(paymentInstrumentDAOMock).save(Mockito.any(PaymentInstrument.class));
     }
 
+    @Test(expected = NeverWantedButInvoked.class)
+    public void deleteKO() {
+        try {
+            paymentInstrumentDAOService.delete("invalid-test");
+        } finally {
+            BDDMockito.verify(paymentInstrumentDAOMock, Mockito.never()).save(Mockito.any(PaymentInstrument.class));
+        }
+    }
+
     @Test
     public void checkActive() {
-        Assert.assertFalse(paymentInstrumentDAOService.checkActive("test", OffsetDateTime.now()));
+        Assert.assertFalse(paymentInstrumentDAOService.checkActive(HASH_PAN, OffsetDateTime.now()));
     }
 }
