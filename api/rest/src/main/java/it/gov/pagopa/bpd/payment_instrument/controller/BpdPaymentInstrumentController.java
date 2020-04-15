732455c384baaf4f39f3ab462a615b3f0a8bdfc3
mdesogus <michael.desogus@everis.com>
2020-04-15 17:08:13 +0200

    Adding @Value annotation on controller class

--- api/rest/src/main/java/it/gov/pagopa/bpd/payment_instrument/controller/BpdPaymentInstrumentController.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/payment_instrument/controller/BpdPaymentInstrumentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.OffsetDateTime;
 
 /**
@@ -24,7 +25,7 @@ public interface BpdPaymentInstrumentController {
 
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    PaymentInstrumentResource update(@PathVariable("id") String hpan, @RequestBody PaymentInstrumentDTO paymentInstrument);
+    PaymentInstrumentResource update(@PathVariable("id") String hpan, @RequestBody @Valid PaymentInstrumentDTO paymentInstrument);
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
--- api/rest/src/main/java/it/gov/pagopa/bpd/payment_instrument/model/PaymentInstrumentDTO.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/payment_instrument/model/PaymentInstrumentDTO.java
@@ -2,11 +2,13 @@ package it.gov.pagopa.bpd.payment_instrument.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
 
 @Data
 public class PaymentInstrumentDTO {
 
+    @NotNull
     private String fiscalCode;
     private OffsetDateTime activationDate;
 
