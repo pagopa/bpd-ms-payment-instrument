6ed2e417ba7db4f10af0a2457c3c1a334e86fa87
Alessio Cialini <acialini@everis.com>
2020-04-15 19:59:16 +0200

    [bpd-payment-instrument/develop] updated api/web with params validation

--- api/rest/src/main/java/it/gov/pagopa/bpd/payment_instrument/controller/BpdPaymentInstrumentController.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/payment_instrument/controller/BpdPaymentInstrumentController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
 
 /**
@@ -20,19 +23,23 @@ public interface BpdPaymentInstrumentController {
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    PaymentInstrumentResource find(@PathVariable("id") String hpan);
+    PaymentInstrumentResource find(@PathVariable("id") @Valid @NotBlank String hpan);
 
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    PaymentInstrumentResource update(@PathVariable("id") String hpan, @RequestBody PaymentInstrumentDTO paymentInstrument);
+    PaymentInstrumentResource update(@PathVariable("id") @Valid @NotBlank String hpan,
+                                     @RequestBody @Valid PaymentInstrumentDTO paymentInstrument);
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void delete(@PathVariable("id") String hpan);
+    void delete(@PathVariable("id") @Valid @NotBlank String hpan);
 
     @GetMapping(value = "/{id}/history", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    boolean checkActive(@PathVariable("id") String hpan, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime accountingDate);
+    boolean checkActive(
+            @PathVariable("id") @Valid @NotBlank String hpan,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            @Valid @NotNull OffsetDateTime accountingDate);
 
 
 }
--- api/rest/src/main/java/it/gov/pagopa/bpd/payment_instrument/model/PaymentInstrumentDTO.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/payment_instrument/model/PaymentInstrumentDTO.java
@@ -2,12 +2,17 @@ package it.gov.pagopa.bpd.payment_instrument.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
 
 @Data
 public class PaymentInstrumentDTO {
 
+    @NotNull
+    @NotBlank
     private String fiscalCode;
+    @NotNull
     private OffsetDateTime activationDate;
 
 }
