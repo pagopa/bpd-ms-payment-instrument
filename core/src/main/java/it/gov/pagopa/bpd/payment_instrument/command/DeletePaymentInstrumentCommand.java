7d6a1bb5c7560bd1df9964109b36d73fe907fd13
alecastagno <alessandro.castagno.sia@gmail.com>
2021-04-27 19:20:40 +0200

    set version

---
+++ core/src/main/java/it/gov/pagopa/bpd/payment_instrument/command/DeletePaymentInstrumentCommand.java
@@ -0,0 +1,10 @@
+package it.gov.pagopa.bpd.payment_instrument.command;
+
+import eu.sia.meda.core.command.Command;
+
+/**
+ * public interface for the DeletePaymentInstrumentCommand extending Meda Command interface
+ */
+
+public interface DeletePaymentInstrumentCommand extends Command<Boolean> {
+}
